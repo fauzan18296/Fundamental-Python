13a769ad77e70133b8c3837e4e7df30165a90143
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-03 22:38:14 +0700

    add: +Materi Fundamental Python If Dan Else Statement

---
+++ 19-IF-Dan-ELSE-Statement/main.py
@@ -0,0 +1,28 @@
+# If dan Else Statement
+
+# 1. if 
+# 2. kondisinya
+# 3. aksinya 
+
+name = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisinya: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if name == 'ucup' : print("Kamu ganteng abieezz!!!!")
+# print(f"Terima kasih {name}")
+
+# 2. program if indentation
+if name == 'ucup':
+  print("Kamu ganteng abiezzzz!!!!!!")
+  print("Kamu juga keren banget!")
+print(f"Terima kasih {name}")
+
+# 3. Else Statement
+if name == 'otong':
+  print("Hai otooong, si keren!!!")
+else:
+  print("Ah kamu bukan otong, kamu ga keren! :(")
+print('akhir dari program')
