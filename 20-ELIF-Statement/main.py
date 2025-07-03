1ad71ec1109db338fd5ddb72da1f96fb0f05c833
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-03 23:23:48 +0700

    add: +Materi Fundamental Python Elif Statement

---
+++ 20-ELIF-Statement/main.py
@@ -0,0 +1,14 @@
+# ELIF => else if statement
+
+name = input("Nama kamu siapa?")
+print(name)
+
+if name == "ucup": # kondisi 1
+  print("hai ganteeng beuds!") # aksi true 1
+elif name == "otong": # kondisi 2
+  print("hai si kece bangeeets!!!!") # aksi true 2
+elif name == "mario": # kondisi 3
+  print("hai humoooooreeeesh!") # aksi true 3
+else:
+  print("au ah gak kenal!!!")
+print("Ini adalah akhir dari program")
