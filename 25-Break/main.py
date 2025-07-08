4eea4718530745fdb186663036e4b95b47e791a2
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-08 22:13:14 +0700

    add: +Materi Fundamental Python Break

---
+++ 25-Break/main.py
@@ -0,0 +1,21 @@
+# Break 
+angka = 0
+while angka < 5:
+  angka += 1
+  print(f"count = {angka}")
+  if angka == 3:
+    print("nice!")
+    break
+  print("wassup!")
+print("cukuup finish!")
+
+data_int = int(input("hitung sampai = "))
+angka = 0
+while True:
+  angka += 1
+  print(f"count = {angka}")
+  if angka == data_int:
+    print("nice!")
+    break
+  print("wassup!")
+print("cukuup finish!")
