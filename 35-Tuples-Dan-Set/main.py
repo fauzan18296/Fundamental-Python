329ed5082f62275714f4e017cd70d839660cc3d8
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-17 14:09:43 +0700

    add: +Materi Fundamental Python Tuples dan Set

---
+++ 35-Tuples-Dan-Set/main.py
@@ -0,0 +1,16 @@
+# List 
+data_list = [10, 2, 4, 3, 2] # Menggunakan kurung siku
+print(data_list)
+# Tuples
+# NOTE: Tuples adalah data saat kita membutuhkan data yang constant atau tidak bisa dirubah
+data_tuples = (7, 8, 9, 10) # Menggunakan kurung biasa atau kurung buka kurung tutup
+print(data_tuples)
+print(data_tuples[1])
+
+# Tidak bisa dilakukan
+# data_tuples[1] = "ucup"
+# data_tuples.append(1)
+
+# Sets (Himpunan)
+data_sets = {10, 4, 3, 2, 4, 7, 6, 5}
+print(data_sets)
