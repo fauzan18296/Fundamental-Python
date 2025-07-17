3cf58aeeb5aad79a654108203a3cefdb6ef13c6e
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-17 14:24:25 +0700

    add: +Materi Fundamental Python Dictionary

---
+++ 36-Dictionary/main.py
@@ -0,0 +1,18 @@
+# List -> array, mengakses dengan menggunakan index
+data_list = ['ucup', 'otong', 'dudung']
+
+print(data_list[0])
+
+# Dictionary (Dict) -> Associative Array
+# Identifier -> Key
+data_dict = {
+  'key' : 'value',
+  'cp' : 'ucup',
+  'tg' : 'otong',
+  'dg' : 'dudung',
+  'number' : 100,
+  'list' : data_list
+}
+print(data_dict['tg'])
+print(data_dict['number'])
+print(data_dict['list'])
