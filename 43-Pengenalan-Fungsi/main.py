329c1fa47ac22a9645c73d9169f80b3aafffff10
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-22 21:20:40 +0700

    add: +Materi Fundamental Python Pengenalan Fungsi

---
+++ 43-Pengenalan-Fungsi/main.py
@@ -0,0 +1,17 @@
+''' Membuat Fungsi '''
+
+def hello_world():
+  ''' Fungsi menampilkan hello world '''
+  print("Hello World")
+  print("Kepada ucup surucup")
+  print("Dan juga kepada otong surotong")
+
+hello_world()
+hello_world()
+
+# fungsi()
+
+def fungsi():
+  ''' Pemanggilan fungsi harus setelah dibuat '''
+  print("ini adalah fungsi")
+fungsi()
