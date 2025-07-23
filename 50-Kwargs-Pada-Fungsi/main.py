e83388f1ab226cf2e607223d6ffa464d5a9b3b15
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-23 21:51:31 +0700

    add: +Materi Fundamental Python **kwargs pada fungsi

---
+++ 50-Kwargs-Pada-Fungsi/main.py
@@ -0,0 +1,33 @@
+'''**kwargs'''
+
+def fungsi(nama, tinggi, berat):
+  '''Fungsi biasa'''
+  print(f"{nama} punya tinggi {tinggi} dan berat {berat}")
+fungsi("ucup", 183, 79)
+
+def fungsi(**kwargs):
+  '''Fungsi kwargs'''
+  nama = kwargs["nama"]
+  tinggi = kwargs["tinggi"]
+  berat = kwargs["berat"]
+  print(f"{nama} punya tinggi {tinggi} dan berat {berat}")
+fungsi(nama = "ucup", tinggi = 183, berat = 79)
+
+# Studi kasus
+def math(*args, **kwargs):
+  output = 0
+  if kwargs["option"] ==  "tambah":
+    for angka in args:
+      output += angka
+  elif kwargs["option"] == "kali":
+    output = 1
+    for angka in args:
+      output *= angka
+  else:
+    print("tidak ada operasi")
+  return output
+
+hasil = math(1, 2, 3, 4, option = "tambah")
+print(f"Hasil jumlah = {hasil}")
+hasil = math(1, 2, 3, 4, option = "kali")
+print(f"Hasil kali = {hasil}")
