325a95e0a406b7852cc87eb98c486ae357871919
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-27 20:47:48 +0700

    add: +Materi Fundamental Python  __Init__.py pada Package

---
+++ 56-Init-Py-Pada-Package/sains/__init__.py
@@ -0,0 +1,9 @@
+from . import matematika # Cara 1
+from . import pisika # Cara 2
+
+# from .matematika import kali # Cara 3
+
+# __all__ = [
+#   "matematika",
+#   "pisika"
+# ] # ! Tidak Disarankan Menggunakan Cara Ini
---
+++ 56-Init-Py-Pada-Package/sains/matematika/__init__.py
@@ -0,0 +1,5 @@
+from . import basic # Cara 1
+from . import scientific # Cara 2
+#  atau
+from .basic import tambah, kali # Cara 3
+from .scientific import pangkat # Cara 4
