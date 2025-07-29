f1e444009297bda3aba6e156503aed7cb4091f83
Ahmad Fauzan <fauzan18296@gmail.com>
2025-07-29 12:45:48 +0700

    add: +Materi Fundamental Python Mengenal PIP

---
+++ 59-Mengenal-PIP/main_app.py
@@ -0,0 +1,5 @@
+# pip list -> Command untuk melihat package atau library yang di install menggunakan pip
+
+# pip -> Command untuk melihat perintah apa saja yang bisa digunakan di pip
+
+# pip uninstall -> Command untuk uninstall package/library yan telah di install oleh pip
