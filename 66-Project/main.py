cf287c2fc95deb6a23c33824b9d4b26ca1ab9968
Ahmad Fauzan <fauzan18296@gmail.com>
2025-08-19 16:27:08 +0700

    add: +Materi Fundamental Python Project Init dan Read

--- 66-Project/main.py
+++ 66-Project/main.py
@@ -1,7 +1,19 @@
 import os
+import CRUD as CRUD
 
 if __name__ == "__main__":
   sistem_operasi = os.name
+
+  match sistem_operasi:
+      case "posix": os.system("clear")
+      case "nt": os.system("cls")
+
+  print("SELAMAT DATANG DI PROGRAM")
+  print("DATABASE PERPUSTAKAAN")
+  print("=========================")
+
+  # Check database itu ada atau tidak
+  CRUD.init_console()
 
   while True:
     match sistem_operasi:
@@ -19,15 +31,12 @@ if __name__ == "__main__":
 
     user_option = input("Masukan opsi: ")
 
-    print("\n======================\n")
-
     match user_option:
-      case "1": print("Read Data")
+      case "1": CRUD.read_console()
       case "2": print("Create Data")
       case "3": print("Update Data")
       case "4": print("Delete Data")
 
-    print("\n======================\n")
     is_done = input("Apakah selesai (y/n)? ")
     if(is_done == "y" or is_done == "Y"):
       break
